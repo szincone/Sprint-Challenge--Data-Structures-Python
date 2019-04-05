d9dbcb59723492fb8bea1caa53639737e728a1aa
szincone <sawyerzincone@gmail.com>
2019-04-05 10:15:05 -0600

    working name duplicate find, .02 sec avg

--- names/names.py
+++ names/names.py
@@ -10,13 +10,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
+# # og sprint code
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates1.append(name_1)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names_1_no_dups = list(set(names_1))
+names_2_no_dups = list(set(names_2))
+names = names_1_no_dups + names_2_no_dups
+names.sort()
+
+for x in range(len(names) - 1):
+    if names[x] == names[x + 1]:
+        duplicates.append(names[x])
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
